cf5ca40860fcef288cc4ef37b038ba856d88377c
Vinhuanc <huann.chenn@gmail.com>
2022-09-07 21:40:52 -0500

    assignment 01 accomplished

--- Assignment03/Assig03-Prog01.cpp
+++ Assignment03/Assig03-Prog01.cpp
@@ -1,37 +1,38 @@
 //Huan Chen, assignment03- program 01
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main(){
-    double adultTicket, childTicket, grossProfit, netProfit, distributorPaid;
+    double adultTicket, childTicket;
     string movieName;
+    double adultTicket1, childTicket1;
     
     cout << "Please enter the movie name: ";
-    cin >> movieName;
-    
-    cout << "Please enter the number of adult tickets sold: ";
-    cin >> adultTicket;
-    adultTicket = adultTicket * 10;
-    
-    cout << "Please enter the number of child tickets sold: ";
-    cin >> childTicket;
-    childTicket = childTicket * 6;
-    
-    cout << "Movie Name: " <<  movieName << "\n";
-    
-    grossProfit = adultTicket + childTicket;
-    
-    
-    netProfit = grossProfit * 0.20;
-    distributorPaid = grossProfit - netProfit;
-    cout << "Adult Tickets Sold: " << adultTicket << "\n";
-    cout << "Child Tickets Sold: " << childTicket << "\n";
-    cout << "Gross Box Office Profit: $" << grossProfit << "\n";
-    cout << "Net Box Office Profit: $" << netProfit << "\n";
-    cout << "Amount Paid to Distributor: $" << distributorPaid << "\n";
-    
-    
-    
-    
 
+    getline(cin, movieName);
+    
+ 
+        cout << "Please enter the number of adult tickets sold: ";
+        cin >> adultTicket1;
+   
+    adultTicket = adultTicket1 * 10;
+        
+        cout << "Please enter the number of child tickets sold: ";
+        cin >> childTicket1;
+        childTicket = childTicket1 * 6;
+        
+        cout << "Movie Name: " << "\"" << movieName << "\"" << "\n";
+    
+        const double grossProfit = adultTicket + childTicket;
+        
+        
+        const double netProfit = grossProfit * 0.20;
+        const double distributorPaid = grossProfit - netProfit;
+        cout << "Adult Tickets Sold: " << adultTicket1 << "\n";
+        cout << "Child Tickets Sold: " << childTicket1 << "\n";
+        cout << "Gross Box Office Profit: $" << setprecision(2) << fixed << grossProfit << "\n";
+        cout << "Net Box Office Profit: $" << netProfit << "\n";
+        cout << "Amount Paid to Distributor: $" << distributorPaid << "\n";
+        
 }
