a2aa10b43729fe607a6dfcc19292dd87aa05736b
Vinhuanc <huann.chenn@gmail.com>
2022-09-08 11:14:35 -0500

    assignment 03 -program 03 is accomplished

--- Assign03-Prog03/main.cpp
+++ Assign03-Prog03/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <string>
 using namespace std;
 
 int main(){
@@ -10,26 +11,26 @@ int main(){
     
     cout << "Please enter the Principal Balance: $" ;
     cin >> principal;
-    
-    
+        
     cout << "Please enter the interest rate (as a decimal) : ";
     cin >> interestRate;
-    //getline(cin,interestRate);
+ 
+    
     
     cout << "Please enter the number of times interest is compounded: " ;
     cin >> interestCompoundRate;
-    
+
     cout << "Interest Rate: " << interestRate * 100 << "%" <<"\n";
-  
+
     cout << "Times Compounded: " << interestCompoundRate <<"\n";
-    
+
     cout << "Principal: $" << setprecision(2) << fixed << principal <<"\n" ;
-    
+
     interests = interestRate * principal;
     updatedSavings = principal * pow((1+(interestRate/interestCompoundRate)),interestCompoundRate);
-    
+
     cout << "Interest: $" << setprecision(2) << fixed << updatedSavings - principal  <<"\n";
-    
-   
+
+
     cout << "Amount in Savings: $" << setprecision(2) << fixed << updatedSavings <<"\n" ;
 }
