c24022cc32d11593af3bf39f5d275c1b28d7e836
Vinhuanc <huann.chenn@gmail.com>
2022-09-08 08:36:35 -0500

    program 03 is working, version before fixing "what if the interest rate is a fraction" section

---
+++ Assign03-Prog03/main.cpp
@@ -0,0 +1,35 @@
+//Assignment03-Program03 Huan Chen
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main(){
+    double principal, interestRate, interests, updatedSavings;
+    int interestCompoundRate;
+    
+    cout << "Please enter the Principal Balance: $" ;
+    cin >> principal;
+    
+    
+    cout << "Please enter the interest rate (as a decimal) : ";
+    cin >> interestRate;
+    //getline(cin,interestRate);
+    
+    cout << "Please enter the number of times interest is compounded: " ;
+    cin >> interestCompoundRate;
+    
+    cout << "Interest Rate: " << interestRate * 100 << "%" <<"\n";
+  
+    cout << "Times Compounded: " << interestCompoundRate <<"\n";
+    
+    cout << "Principal: $" << setprecision(2) << fixed << principal <<"\n" ;
+    
+    interests = interestRate * principal;
+    updatedSavings = principal * pow((1+(interestRate/interestCompoundRate)),interestCompoundRate);
+    
+    cout << "Interest: $" << setprecision(2) << fixed << updatedSavings - principal  <<"\n";
+    
+   
+    cout << "Amount in Savings: $" << setprecision(2) << fixed << updatedSavings <<"\n" ;
+}
