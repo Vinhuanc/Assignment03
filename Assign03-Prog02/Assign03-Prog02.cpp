1ab1fedcff75ff0c31a361ab694e964a05cba055
Vinhuanc <102390736+Vinhuanc@users.noreply.github.com>
2022-09-22 20:22:49 -0500

    Update Assign03-Prog02.cpp

--- Assign03-Prog02/Assign03-Prog02.cpp
+++ Assign03-Prog02/Assign03-Prog02.cpp
@@ -1,4 +1,26 @@
 //Huan Chen, assignment03- program 01
+// Program #2
+// A retail company must file a monthly sales tax report listing the sales for the month and the amount of sales tax collected. Write a program that asks for the month, the year, and the total amount collected at the cash register (that is, sales plus sales tax). Assume the state sales tax is 4 percent and the county sales tax is 2 percent.
+// If the total amount collected is known and the total sales tax is 6 percent, the amount of product sales may be calculated as: S = (T/1.06). S is the product sales and T is the total income.
+// • Display the year next to the month as shown in the example below
+// • Use const for the tax rates
+// Sample Execution:
+// Please enter the Month: January
+// Please enter the year: 2017
+// Please enter the total amount collected: $482
+// Month: January 2017
+// --------------------
+// Total Collected:        $482.00
+// Sales:                  $454.72
+// County Sales Tax:       $9.09
+// State Sales Tax:        $18.19
+// Total Sales Tax:        $27.28
+// Press any key to continue . . .
+// Things to Test/verify:
+// - What happens if the month name is more than a single word?
+// - What happens if the year is not entered as an integer?
+// - Are your outputs formatted for their units?
+// - Did you use constants as specified?
 #include <iostream>
 #include <iomanip>
 #include <cmath>
